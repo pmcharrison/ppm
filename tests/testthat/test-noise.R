71758018bcdcccea7719a8a873b2d4886d8f854d
Peter Harrison <pmc.harrison@gmail.com>
2019-03-13 16:02:07 +0000

    Adding test for noise

---
+++ tests/testthat/test-noise.R
@@ -0,0 +1,23 @@
+context("test-noise")
+
+test_that("misc", {
+  x <- new_ppm_decay(20, noise = 0.1)
+  for (i in seq_len(100000)) {
+    x$insert(1:3, 0L, 0L, FALSE)
+  }
+  df <- x$as_tibble()
+  df$n <- vapply(df$pos, length, integer(1))
+  df$char <- vapply(df$n_gram, function(x) paste(letters[x], collapse = ""), character(1))
+  df$dist <- as.integer(adist("abc", df$char))
+  
+  prop_exact <- sum(df$n[df$dist == 0]) / sum(df$n)
+  prop_1_diff <- sum(df$n[df$dist == 1]) / sum(df$n)
+  prop_2_diff <- sum(df$n[df$dist == 2]) / sum(df$n)
+  prop_3_diff <- sum(df$n[df$dist == 3]) / sum(df$n)
+  
+  expect_equal(prop_exact, (0.9 + 0.1 / 20) ^ 3, tolerance = 0.01)
+  expect_equal(prop_1_diff, 3 * ((0.1 * 19 / 20) * (0.9 + 0.1 / 20) ^ 2), tolerance = 0.01)
+  expect_equal(prop_2_diff, 3 * ((0.1 * 19 / 20) ^ 2 * (0.9 + 0.1 / 20)), tolerance = 0.01)
+  expect_equal(prop_3_diff, (0.1 * 19 / 20) ^ 3, tolerance = 0.01)
+
+})
